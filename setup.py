551fb6dab92de8f6fb86786a828287e8e92847f7
Tim Wheeler <twheele3@uoregon.edu>
2024-10-09 15:40:31 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Tim Wheeler',
     author_email='twheele3@uoregon.edu',
     packages=['tiletif'],
-    install_requires=['numpy','os','tifffile'],
+    install_requires=['numpy','tifffile'],
     version='0.1',
     license='MIT',
     description='A package to tile large N-dimensional tiff files through memmapping for ease of processing.',
